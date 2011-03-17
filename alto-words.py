52eeb707fb0dc894c8ee5e6b3d72ad3b930201d8
Ed Summers <ehs@pobox.com>
2011-03-16 23:18:55 -0400

    need to store open dictionary correctly

--- alto-words.py
+++ alto-words.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 """
-ocr.py reads an Alto OCR XML file and prints out the ratio of dictionary words 
-to all words for the document
+alto-words.py reads an Alto OCR XML file and prints out the ratio of 
+dictionary words to all words for the document
 """
 
 import re
@@ -25,20 +25,21 @@ def main(ocr_filename):
 class AltoHandler(ContentHandler):
 
     def __init__(self):
+        self.dictionary = dbm.open('dictionary')
         self.dictionary_words = []
         self.words = []
-        self.dictionary = dbm.open('dictionary')
 
     def startElement(self, tag, attrs):
         if tag == 'String':
             word = attrs.get("CONTENT").lower()
-            if dictionary.has_key(word):
+            print word
+            if self.dictionary.has_key(word):
                 self.dictionary_words.append(word)
             self.words.append(word)
 
     @property
     def ratio(self):
-        return self.dictionary_words / float(self.words)
+        return len(self.dictionary_words) / float(len(self.words))
 
 if __name__ == "__main__":
     filename = sys.argv[1]
